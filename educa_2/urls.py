30771e2e01d37618d7ad68e8b3f0f8fc937f467a
cova94 <masterc890@gmail.com>
2023-04-18 14:31:26 -0500

    Finished the rest of chapter 13, and made some progress on chapter 14.

--- educa_2/urls.py
+++ educa_2/urls.py
@@ -18,12 +18,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from courses.views import CourseDetailView
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
+    path('', CourseDetailView.as_view(), name='course_list'),
 ]
 
 if settings.DEBUG:
